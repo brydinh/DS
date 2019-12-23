9152016cfe29659e0346249c9cd2f3d56635c382
brian dinh <bdinh98@gmail.com>
2019-12-23 16:42:46 -0500

    createNode

--- BinaryTree.c
+++ BinaryTree.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Binary Tree Node
+struct BTNode
+{
+    int data;
+    struct BTNode* leftChild;
+    struct BTNode* rightChild;
+};
+
+// Binary Tree Root
+struct BinaryTree
+{
+    struct BTNode* root;
+};
+
+
+struct BTNode* createNode(int num)
+{
+    struct BTNode* node = (struct LinkedList*)malloc(sizeof(struct LinkedList));
+    
+    BTNode->data = num;
+    BTNode->leftChild = NULL;
+    BTNode->rightChild = NULL;
+
+    return node;
+}
 
 int main()
 {
