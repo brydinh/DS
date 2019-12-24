8c355bcbc431d0e8a41ff8e79d6d7303fa9d9b2b
brian dinh <bdinh98@gmail.com>
2019-12-23 22:54:20 -0500

    figure out why seg fault tmw

--- BinarySearchTree.c
+++ BinarySearchTree.c
@@ -15,23 +15,56 @@ struct BinarySearchTree
     struct BSTNode* root;
 };
 
-void insert(struct BinarySearchTree* b, int data)
-{
-    
-}
-
 struct BSTNode* createNode(int num)
 {
     struct BSTNode* node = (struct BSTNode*)malloc(sizeof(struct BSTNode));
     
-    BSTNode->data = num;
-    BSTNode->leftChild = NULL;
-    BSTNode->rightChild = NULL;
+    node->data = num;
+    node->leftChild = NULL;
+    node->rightChild = NULL;
 
     return node;
 }
 
+void insertNode(struct BSTNode* root, int data)
+{
+    printf("in inserNode");
+    if(root == NULL)
+    {
+        struct BSTNode* insertThis = createNode(data);
+        root = insertThis;
+    }
+    else
+    {
+        if(root->data >= data)
+        {
+            insertNode(root->rightChild, data);
+        }
+        else
+        {
+            insertNode(root->leftChild, data);
+        }  
+    }
+}   
+
+struct BinarySearchTree* createRoot()
+{
+    struct BinarySearchTree* b = (struct BinarySearchTree*)malloc(sizeof(struct BinarySearchTree));
+    b->root = NULL;
+    return b;
+}
+
+
 int main()
 {
+    struct BinarySearchTree* b = createRoot();
+
+    insertNode(b->root, 5);
+    //insertNode(b->root, 2);
+
+    printf("%d\n", b->root->data);
+
+   // printf("%d\n", b->root->leftChild->data);
+
     return 0; 
 }
