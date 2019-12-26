56a2c204e4dc0b617a8643dd014d5cd8a569c016
brian dinh <bdinh98@gmail.com>
2019-12-25 20:31:47 -0500

    implemented postorder, inorder, and postorder traversels alongside isleaf

--- BinarySearchTree.c
+++ BinarySearchTree.c
@@ -37,7 +37,56 @@ struct BSTNode* insertNode(struct BSTNode* root, int data)
     }
 
     return root;
-}   
+}
+
+// Left, Root, Right
+void PrintTreeInorder(struct BSTNode* root)
+{
+    if(root == NULL) return;
+    
+    PrintTreeInorder(root->leftChild); 
+    printf("%d, \t", root->data); 
+    PrintTreeInorder(root->rightChild); 
+}
+
+// Left, Right, Root
+void PrintTreePostorder(struct BSTNode* root)
+{
+    if(root == NULL) return;
+
+    PrintTreePostorder(root->leftChild); 
+    PrintTreePostorder(root->rightChild); 
+    printf("%d, \t", root->data);    
+}
+
+// Root, Left, Right
+void PrintTreePreorder(struct BSTNode* root)
+{
+    if(root == NULL) return;
+    
+    printf("%d, \t", root->data); 
+    PrintTreePreorder(root->leftChild); 
+    PrintTreePreorder(root->rightChild);   
+}
+
+void isLeaf(struct BSTNode* root , int findThis)
+{
+    if(root == NULL) return;
+
+    if(root->data == findThis)
+    {
+        if(root->leftChild == NULL && root->rightChild == NULL)
+        {
+            printf("is leaf!\n");
+        }
+        else
+        {
+            printf("is not leaf!\n");
+        }      
+    }
+    isLeaf(root->leftChild, findThis);
+    isLeaf(root->rightChild, findThis);
+}
 
 
 int main()
@@ -46,11 +95,23 @@ int main()
 
     root1 = insertNode(root1, 5);
     root1 = insertNode(root1, 1);
+    root1 = insertNode(root1, 6);
+    root1 = insertNode(root1, 0);
+    root1 = insertNode(root1, 3);
 
+    printf("Preorder: ");
+    PrintTreePreorder(root1);
+    printf("\n");
+    printf("Inorder: ");
+    PrintTreeInorder(root1);
+    printf("\n");
+    printf("Postorder: ");
+    PrintTreePostorder(root1);
+    printf("\n");
 
-    printf("%d\n", root1->data);
-
-    printf("%d\n", root1->leftChild->data);
+    isLeaf(root1, 1); // should be "is not leaf"
+    isLeaf(root1, 0); // should be "is leaf"
+    isLeaf(root1, 6); // should be "is leaf"
 
     return 0; 
 }
